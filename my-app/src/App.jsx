4caf9582df5a811dae9af73d78f800c18a82e323
rgib <regangib@btinternet.com>
2025-10-23 19:56:18 +0100

    New Commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <Title/>
+      <Header/>
+      <Main/>
+      </>
+       
+  )
+}
+
+function Title(){
+  return(
+    <p>Hello world</p>
+  )
+}
+
+function Header(){
+  return(
+    <h1>Regan Gibson</h1>
+  )
+}
+
+function Main() {
+  return(
+    <p>Welcome to my Photography page</p>
+  )
+}
+
+export default App
