d2dbbd0c69835a0517e0f5e9b2b99efa6f059bfd
rgib <regangib@btinternet.com>
2025-10-28 20:44:09 +0000

    TravelJournal Commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,7 +10,52 @@ function App() {
       <>
             <Header />
             <main className="container">
-                <Entry />
+                <Entry 
+                img="../images/montenagro.JPG"
+                name="Kotor"
+                img2="../images/pin.png"
+                location = "Montenagro"
+                link = "https://www.google.com/maps/place/Kotor,+Montenegro/@42.4242999,18.7705761,1805m/data=!3m2!1e3!4b1!4m6!3m5!1s0x134c33063d70c91b:0x7a73f15e212e9306!8m2!3d42.424662!4d18.771234!16zL20vMDFwam4w?entry=ttu&g_ep=EgoyMDI1MTAyMi4wIKXMDSoASAFQAw%3D%3D"
+                date= "12 June, 2025 - 19 June, 2025"
+                information="Kotor, Montenegro, is a stunning coastal town set on the Adriatic Sea, famous for its well-preserved medieval old town, winding stone streets, and dramatic bay surrounded by steep mountains. Enclosed by ancient Venetian walls, Kotor offers a blend of history, culture, and breathtaking scenery — a UNESCO World Heritage Site that feels like a step back in time."
+                />
+
+                
+              <Entry
+                img="../images/Bulgaria.jpg"
+                name="Borovets"
+                img2="../images/pin.png"
+                location = "Bulgaria"
+                link = "https://www.google.com/maps/place/2010+%D0%91%D0%BE%D1%80%D0%BE%D0%B2%D0%B5%D1%86,+Bulgaria/@42.2724875,23.5855834,7246m/data=!3m2!1e3!4b1!4m6!3m5!1s0x14ab3c9b6660506b:0xa00a014cd0f8790!8m2!3d42.2706675!4d23.6056165!16zL20vMDloem1n?entry=ttu&g_ep=EgoyMDI1MTAyNi4wIKXMDSoASAFQAw%3D%3D"
+                date= "1 January, 2025 - 7 January, 2025"
+                information="Borovets is a mountain resort in Bulgaria, located on the northern slopes of the Rila Mountains, within the municipality of Samokov in Sofia Province.
+                            It sits at an elevation of around 1,300 m at its base, with ski slopes reaching up to around 2,500 m. 
+                            It is approximately 70–75 km from the Bulgarian capital, Sofia
+"
+              
+              />
+              <Entry
+                img="../images/Mallorca.JPG"
+                img2="../images/pin.png"
+                name="Cal'a Dor"
+                location = "Mallorca"
+                link = "https://www.google.com/maps/place/Cala+d'Or,+Balearic+Islands,+Spain/@39.3698163,3.2074569,7570m/data=!3m2!1e3!4b1!4m6!3m5!1s0x129650c363e848bb:0xa02af717f574100!8m2!3d39.372606!4d3.22914!16s%2Fm%2F027dfzz?entry=ttu&g_ep=EgoyMDI1MTAyNi4wIKXMDSoASAFQAw%3D%3D"
+                date= "13 September, 2025 - 20 September, 2025"
+                information="Cala d’Or is a picturesque resort on Mallorca’s southeast coast, known for its whitewashed buildings, stylish marina, and series of small sandy coves like Cala Gran and Cala Esmeralda. It offers a relaxed, family-friendly atmosphere with plenty of restaurants, bars, and shops, plus water sports and boat trips from the harbor. The area is more peaceful than Mallorca’s party resorts, making it ideal for couples and families. It’s about a 45–60-minute drive from Palma Airport."
+              
+              />
+              
+              <Entry
+                img="../images/lagos.jpg"
+                img2="../images/pin.png"
+                location = "Portugal"
+                name="Lagos"
+                link = "https://www.google.com/maps/place/Lagos,+Portugal/@37.1152515,-8.7654046,31234m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd1b304950234d8d:0x5410aaa3471afc57!8m2!3d37.1027881!4d-8.6730275!16zL20vMDI4anhq?entry=ttu&g_ep=EgoyMDI1MTAyNi4wIKXMDSoASAFQAw%3D%3D"
+                date= "2 July, 2024 - 9 July, 2024"
+                information="Lagos is a historic coastal town in Portugal’s Algarve region, famous for its stunning cliffs, golden beaches, and cobbled old town streets. Highlights include Praia Dona Ana and Ponta da Piedade, known for dramatic rock formations and turquoise waters. The town blends a relaxed beach vibe with vibrant nightlife, great seafood restaurants, and rich maritime history. Lagos is ideal for couples, families, and surfers alike, offering both scenic beauty and cultural charm, about 1.5 hours’ drive from Faro Airport."
+              
+              />
+
             </main>
         </> 
   )
--- my-app/src/components/Entry.jsx
+++ my-app/src/components/Entry.jsx
@@ -1,17 +1,18 @@
-export default function Entry () {
+export default function Entry (props) {
+    console.log(props)
     return(
 
         <article className="journal-entry">
          <div className="main-image-container">
-            <img className="main-image" src="../images/montenagro.JPG" alt="montenagro" />
+            <img className="main-image" src={props.img} alt="montenagro" />
             </div>
            <div className="info-container">
              <img className="marker" src="../images/pin.png" alt="marker"  />
-            <span className="country">Montenagro</span>
-             <a href="https://www.google.com/maps/place/Kotor,+Montenegro/@42.4242999,18.7705761,1805m/data=!3m2!1e3!4b1!4m6!3m5!1s0x134c33063d70c91b:0x7a73f15e212e9306!8m2!3d42.424662!4d18.771234!16zL20vMDFwam4w?entry=ttu&g_ep=EgoyMDI1MTAyMi4wIKXMDSoASAFQAw%3D%3D">View on Google Maps</a>
-             <h2 className="entry-title">Kotor</h2>
-            <p className="trip-dates">12 July, 2025 - 24 July, 2025</p>
-            <p className="entry-text">Kotor, Montenegro, is a stunning coastal town set on the Adriatic Sea, famous for its well-preserved medieval old town, winding stone streets, and dramatic bay surrounded by steep mountains. Enclosed by ancient Venetian walls, Kotor offers a blend of history, culture, and breathtaking scenery — a UNESCO World Heritage Site that feels like a step back in time.</p>
+            <span className="country">{props.name}</span>
+             <a href= {props.link}>View on Google Maps</a>
+             <h2 className="entry-title">{props.location}</h2>
+            <p className="trip-dates">{props.date}</p>
+            <p className="entry-text">{props.information}</p>
             </div>     
             </article>
      
