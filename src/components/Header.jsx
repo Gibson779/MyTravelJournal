039956d328450642429078995044cc0d9a94e881
rgib <regangib@btinternet.com>
2025-12-01 20:45:15 +0000

    Travel Journal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
       <header>
         <img src="../images/earth.png" alt="globe icon" />
-        <h1>my travel journal</h1>
+        <h1>Travel Jouranl</h1>
         </header>
 
     )
