a07b97193f4f0e93badd6e32ffdc330ae32e04d7
rgib <regangib@btinternet.com>
2025-12-09 19:45:11 +0000

    update Entry.jsx

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,13 +1,18 @@
+
+
+
+
+
 export default function Entry (props) {
-    
+     
     return(
 
         <article className="journal-entry">
          <div className="main-image-container">
             <img 
                 className="main-image" 
-                src={props.img.src}
-                alt={props.img.alt}
+                src={props.entry.img.src}
+                alt={props.entry.img.alt}
          />
 
             </div>
@@ -19,11 +24,11 @@ export default function Entry (props) {
                 
         />
         
-            <span className="country">{props.country}</span>
-             <a href= {props.googleMapsLink}>View on Google Maps</a>
-             <h2 className="entry-title">{props.title}</h2>
-            <p className="trip-dates">{props.date}</p>
-            <p className="entry-text">{props.text}</p>
+            <span className="country">{props.entry.country}</span>
+             <a href= {props.entry.googleMapsLink}>View on Google Maps</a>
+             <h2 className="entry-title">{props.entry.title}</h2>
+            <p className="trip-dates">{props.entry.date}</p>
+            <p className="entry-text">{props.entry.text}</p>
             </div>     
             </article>
      
